a10794694df2eb34fca726b0a20a40ed72e2751e
oshopina <olashopina@gmail.com>
2024-05-01 12:22:00 +1000

    Olga updated plot for subgroups

--- 18S/Scripts/Supergroup_heatmap.R
+++ 18S/Scripts/Supergroup_heatmap.R
@@ -6,24 +6,70 @@ env <- read.csv('18S/Data/env_16S.csv', row.names = 1)
 env <- env[order(env$pH),] ## Sort the way you want the samples to be ordered on heatmap
 env = env[rownames(env) %in% colnames(otu),]
 
-collapsed_otu = aggregate(otu[,1:98], by = list(Supergroup = otu$Supergroup), FUN = 'sum')
-rownames(collapsed_otu) = collapsed_otu$Supergroup
+collapsed_otu = aggregate(otu[,1:98], by = list(Subdivision = otu$Subdivision), FUN = 'sum')
+rownames(collapsed_otu) = collapsed_otu$Subdivision
 collapsed_otu = collapsed_otu[,-1] |> t() |> as.data.frame()
-collapsed_otu$Other = collapsed_otu$Eukaryota_X + collapsed_otu$Haptista + collapsed_otu$Provora
-collapsed_otu = collapsed_otu[,c(1:3, 5, 7, 9, 10)]
 
 collapsed_otu <- collapsed_otu[rownames(env),]
-anova <- auto_aov_fixed(collapsed_otu, ~ pH, env_df = env)$Results
+df = collapsed_otu
+
+############################### Change point analysis ##########################
+df_mat = df |> as.matrix()
+cpt = geomcp(df_mat)
+plot(cpt)
+
+dist_cpt = cpt.meanvar(
+  distance(cpt),
+  method = "PELT",
+  penalty = 'CROPS',
+  pen.value = c(5, 500)
+)
+
+pen.value.full(dist_cpt)
+dist_var = cpts.full(dist_cpt)
+tail(dist_var)
+plot(dist_cpt, diagnostic = T)
+plot(dist_cpt, ncpts = 2)
+
+######################### WRITE CHOSEN NUMBER OF CHANGEPOINTS FOR ANGLE AND DISTANCE
+cp_dist = readline('Number of changepoint for distance data: ') |> as.numeric()
+
+ang_cpt = cpt.meanvar(
+  angle(cpt),
+  method = "PELT",
+  penalty = 'CROPS',
+  pen.value = c(5, 500)
+)
+
+pen.value.full(ang_cpt)
+ang_var = cpts.full(ang_cpt)
+tail(ang_var)
+plot(ang_cpt, diagnostic = T)
+plot(ang_cpt, ncpts = 2)
+######################### WRITE CHOSEN NUMBER OF CHANGEPOINTS FOR ANGLE AND DISTANCE
+cp_angle = readline('Number of changepoint for angle data: ') |> as.numeric()
+
+############################# Create tax labels ###############################
+tax = otu[otu$Subdivision %in% colnames(collapsed_otu),]
+tax = tax[, 100:102]
+tax = unique(tax)
+tax <- tax[order(tax$Supergroup, tax$Division, tax$Subdivision),] 
+tax$d_sd_label <- paste0('d_', tax$Division, ';sd_', tax$Subdivision)
+rownames(tax) = tax$d_sd_label
+df <- df[,tax$Subdivision]
+
+################################# ANOVA ##################################
+anova <- auto_aov_fixed(df, ~ pH, env_df = env)$Results
 anova = subset(anova, str_detect(Parameter, 'pH'))[, c('Data', 'F_value', 'p_value', 'Signif')]
 rownames(anova) = anova$Data
-anova = anova[colnames(collapsed_otu),]
+anova = anova[colnames(df),]
 anova$F_value = round(anova$F_value, digits = 2)
 anova$p_value = round(anova$p_value, digits = 4)
 anova$Signif <- gsub("\\*+", "*", anova$Signif)
 
+colnames(df) = rownames(tax)
 
-
-df_scaled <- collapsed_otu |> vegan::decostand(method = 'normalize', MARGIN = 2) |> scale() |> t()
+df_scaled <- df|> vegan::decostand(method = 'normalize', MARGIN = 2) |> scale() |> t()
 
 my_palette = colorRampPalette(c(
   'white',
@@ -77,17 +123,94 @@ ha <- HeatmapAnnotation(
 )
 
 ha_f <- rowAnnotation(
-  anova = anno_text(anova$Signif)
+  anova = anno_text(anova$Signif),
+  d_sd = anno_text(tax$d_sd_label)
 )
 
-Heatmap(
+## Create additional annotation for change point frequency
+ha2 = HeatmapAnnotation(
+  change_point_dist = anno_lines(
+    data.set(dist_cpt),
+    axis = F
+  ),
+  change_point_ang = anno_lines(
+    data.set(ang_cpt),
+    axis = F
+  ),
+  height = unit(3, "cm"), 
+  show_annotation_name = F
+)
+
+draw(Heatmap(
   df_scaled,
+  row_split = tax$Supergroup,
+  row_title_rot = 0,
+  row_title_gp = gpar(fontsize = 10, fontface = 'bold'),
   row_order = rownames(df_scaled),
   column_order = env$Hoosfield.ID,
   col = my_palette(100),
   show_column_names = FALSE,
-  row_title = NULL,
+  show_row_names = F,
   bottom_annotation = ha,
-  left_annotation = ha_f,
+  right_annotation = ha_f,
+  top_annotation = ha2,
   show_heatmap_legend = FALSE
-)
+))
+
+change_points_dist = cpts(dist_cpt, cp_dist)[!is.na(cpts(dist_cpt, cp_dist))]
+
+#creating vertical line for each change point in distance
+for(i in change_points_dist) {
+  decorate_annotation("change_point_dist", {
+    grid.lines(
+      x = unit(c(i, i), 'native'),
+      y = unit(c(min(
+        data.set(dist_cpt)
+      ), max(
+        data.set(dist_cpt)
+      )), 'native'),
+      gp = gpar(col = "red", lwd = 3)
+    )
+  })
+}
+
+#Adding title
+decorate_annotation("change_point_dist", {
+  grid.text(
+    "Mean\nchange\npoint",
+    x = unit(0, "npc"),
+    y = unit(0.5, "npc"),
+    rot = 90,
+    vjust = -0.1, 
+    gp = gpar(fontsize = 9)
+  )
+})
+
+change_points_ang = cpts(ang_cpt, cp_angle)[!is.na(cpts(ang_cpt, cp_angle))]
+
+#creating vertical line for each change point in angle
+for(i in change_points_ang) {
+  decorate_annotation("change_point_ang", {
+    grid.lines(
+      x = unit(c(i, i), 'native'),
+      y = unit(c(min(
+        data.set(ang_cpt)
+      ), max(
+        data.set(ang_cpt)
+      )), 'native'),
+      gp = gpar(col = "red", lwd = 3)
+    )
+  })
+}
+
+#Adding title
+decorate_annotation("change_point_ang", {
+  grid.text(
+    "Variance\nchange\npoint",
+    x = unit(0, "npc"),
+    y = unit(0.5, "npc"),
+    rot = 90,
+    vjust = -0.1,
+    gp = gpar(fontsize = 9)
+  )
+})
